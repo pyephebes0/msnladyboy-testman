be7a73d45525ab2ea035184d0c54b92479494834
pyephebes0 <xyz928.pyephebes@gmail.com>
2025-04-17 00:32:18 +0700

    Deploy to Koyeb

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -10,11 +10,10 @@
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
+      credentials: "include",
     });
  
     if (res.ok) {
-			const data = await res.json();
-			document.cookie = `token=${data.token}; path=/`; // ✅ Save token in cookie
 			goto('/profile'); // ✅ Redirect after login
 		} else {
 			const errorData = await res.json();
